26019934baf3bb5964a9117505e547058a49c18c
kanade <kanade2960deel@gmail.com>
2024-11-24 06:10:25 +0000

    課題１６を作った

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,12 +16,30 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+       
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+     
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
 
     public function update()
